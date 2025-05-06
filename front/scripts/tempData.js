9e641e5724de37c36f781cda511ffc9d23ba16d2
Carlosf12 <carlosfigueira1212@gmail.com>
2025-05-06 18:12:59 -0400

    feat(front): Render initial movie data from tempData and organize basic frontend structure

---
+++ front/scripts/tempData.js
@@ -0,0 +1,29 @@
+const tempData = [
+    {
+      "title": "The Shawshank Redemption",
+      "year": 1994,
+      "director": "Frank Darabont",
+      "duration": "2h 22min",
+      "genre": ["Drama", "Crime"],
+      "rate": 9.3,
+      "poster": "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhZC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_QL75_UX190_CR0,0,190,281_.jpg"
+    },
+    {
+      "title": "The Godfather",
+      "year": 1972,
+      "director": "Francis Ford Coppola",
+      "duration": "2h 55min",
+      "genre": ["Drama", "Crime"],
+      "rate": 9.2,
+      "poster": "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODYzZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UX190_CR0,0,190,281_.jpg"
+    },
+    {
+      "title": "Pulp Fiction",
+      "year": 1994,
+      "director": "Quentin Tarantino",
+      "duration": "2h 34min",
+      "genre": ["Crime", "Drama"],
+      "rate": 8.9,
+      "poster": "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRmLWFmYjItNzlmYTkzMGU0MGNjXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UX190_CR0,0,190,281_.jpg"
+    }
+  ]
